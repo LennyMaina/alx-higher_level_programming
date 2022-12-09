0ee88813b30b6060722aed2c732da44aa7be88a0
LennyMaina <lennymaina77@gmail.com>
2022-12-09 02:02:25 -0800

    Check later

---
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+def search_replace(my_list, search, replace):
+
+        length = len(my_list)
+
+            new_list = []
+
+                for i in my_list:
+
+                            if i == search:
+
+                                            i = replace
+
+                                                        new_list.append(i)
+
+                                                                else:
+
+                                                                                new_list.append(i)
+
+                                                                                    return new_list
