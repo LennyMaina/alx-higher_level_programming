1051a6c9738fc34e29a251c309f7209b9ad0a142
LennyMaina <111064242+LennyMaina@users.noreply.github.com>
2022-12-20 23:07:49 +0300

    Create 1-square.py

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Class that defines a square"""
+
+
+class Square:
+    """Declares a square"""
+
+
+    def __init__(self, size):
+        """Initialize a square
+
+            Args:
+                Size: Size of Square
+
+
+        """
+        self.__size = size
